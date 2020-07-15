8af57234413d6ca32d8a7873e68cb5fe15269daa
Hao-Ping Lin <pinglin927@gmail.com>
2020-07-15 21:11:10 +0800

    Update notes

--- Sort/heap_sort.cpp
+++ Sort/heap_sort.cpp
@@ -1,63 +1,57 @@
 /*
  * 堆排序
  *
- * 1. 建max heap
- * 2. 把根結點移到末節點
- * 3. 更新max heap
- * 4. 重複2~4
- *
- * T: O(n); S: O(h)
+ * T: O(n); S: O(1)
  */
 
 #include <iostream>
 using namespace std;
 
-// 更新max heap
+// 往下更新節點
 void AdjustDown(int arr[], int k, int len) {
-    // 紀錄父節點
+    // 備份父節點
     int temp = arr[k];
-    for (int i = 2 * k; i < len; i *= 2) {
-        // 取出子節點較大者
-        if (arr[i] < arr[i + 1]) i++;
-        // 若父節點比節點大，結束
+    for (int i = 2 * k + 1; i < len; i = i * 2 + 1) {
+        // 取出左右子節點較大者(注意可能沒有右節點)
+        if (i + 1 < len && arr[i + 1] > arr[i]) i++;
+        // 若父節點大於子節點，結束
         if (temp >= arr[i]) break;
-        // 反之，替換
         else {
+            // 若小於父節點，交換位置
             arr[k] = arr[i];
-            // 更新舊父節點的新位置
+            // 紀錄交換位置後的父節點位置
             k = i;
         }
     }
-    // 將父節點放進最後適合的位置
+    // 將父節點丟入最後位置
     arr[k] = temp;
 }
 
 // 建max heap
 void BuildMaxHeap(int arr[], int len) {
-    // 從最後一個父節點開始操作
-    for (int i = len / 2; i >= 0; i--) {
+    for (int i = len / 2 - 1; i >= 0; i--) {
         AdjustDown(arr, i, len);
-    }
+    } 
 }
 
 // 排序max heap
 void heap_sort(int arr[], int len) {
     // 建max heap
     BuildMaxHeap(arr, len);
-    // 排列max heap
     for (int i = len - 1; i >= 0; i--) {
-        // 把根節點(最大的節點)移到最後一個位置
-        swap(arr[i], arr[0]);
-        // 重新排列max heap，但每回合都縮減一個尾巴元素(因為尾巴已經排序過了)
-        AdjustDown(arr, 0, i - 1);
+        // 把根節點(最大點)與最末點交換
+        swap(arr[0], arr[i]);
+        // 重新更新max heap，注意要忽略已排序的位置
+        AdjustDown(arr, 0, i);
     }
 }
 
+
 int main()
 {
-    int arr[10] = {1,10,2,9,3,8,4,7,5,6};
-    heap_sort(arr, 10);
-    for (int i = 0; i < 10; i++) {
+    int arr[5] = {2,3,4,5,8};
+    heap_sort(arr, 5);
+    for (int i = 0; i < 5; i++) {
         cout << arr[i] << "\t";
     }
     cout << endl;
---
+++ Sort/merge_sort.cpp
@@ -0,0 +1,53 @@
+/*
+ * 歸併排序
+ *
+ * 1. 將數組從中點拆開數個子數組，子數組最多有2個元素
+ * 2. 使用另一數組儲存每個相鄰的子數組
+ * 3. 兩數組互比較元素後填入原數組
+ *
+ * T: 每趟歸併O(n)，共需logn趟，結果O(nlogn); S: O(n)
+ * */
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(int arr[], int low, int mid, int high) {
+    // 設一數組來接原數組
+    vector<int> b;
+    for (int i = 0; i <= high; i++) b.push_back(arr[i]);
+    // 交叉比較兩子樹組並填入原數組
+    int i = low, j = mid + 1, k = low;
+    for (; i <= mid && j <= high; k++) {
+        if (b[i] <= b[j]) arr[k] = b[i++];
+        else arr[k] = b[j++];
+    }
+    // 若任一數組尚有元素，將其插入原數組
+    while (i <= mid) arr[k++] = b[i++];
+    while (j <= high) arr[k++] = b[j++];
+}
+
+void merge_sort(int arr[], int low, int high) {
+    // 若數組至少有2個元素
+    if (low < high) {
+        // 取得中點
+        int m = low + (high - low) / 2;
+        // 拆成左右子數組
+        merge_sort(arr, low, m);
+        merge_sort(arr, m + 1, high);
+        // 合併排列左右子數組
+        merge(arr, low, m, high);
+    }
+} 
+
+int main()
+{
+    int arr[10] = {1,10,2,9,3,8,4,7,5,6};
+    merge_sort(arr, 0, 9);
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+    return 0;
+}
