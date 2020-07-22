dba22dd121b011d3014626aaa04ee821c8a61e38
Hao-Ping Lin <pinglin927@gmail.com>
2020-07-22 21:43:12 +0800

    update notes

--- Sort/select_sort.cpp
+++ Sort/select_sort.cpp
@@ -1,7 +1,7 @@
 /*
- * ��ܱƧ�
+ * 選擇排序
  *
- * �C�������̥��䪺��������ǭȡA�ñq�Ѿl����������h�̤p���P�ۤv�洫
+ * 每次紀錄最左邊的元素當基準值，並從剩餘的元素中選則最小的與自己交換
  *
  * T: O(n^2); S: O(1)
  */
@@ -10,19 +10,19 @@
 using namespace std;
 
 void select_sort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {       // �@�@�i��n-1��
-        int min = i;                        // �����̤p������m
-        for (int j = i + 1; j < n; j++) {   // �bi~n����̤ܳp����
-            if (arr[j] < arr[min]) min = j; // ��s�̤p������m
+    for (int i = 0; i < n - 1; i++) {       // 一共進行n-1趟
+        int min = i;                        // 紀錄最小元素位置
+        for (int j = i + 1; j < n; j++) {   // 在i~n中選擇最小元素
+            if (arr[j] < arr[min]) min = j; // 更新最小元素位置
         }
-        swap(arr[i], arr[min]);             // �P��i�Ӧ�m�洫
+        swap(arr[i], arr[min]);             // 與第i個位置交換
     }
 }
 
 int main()
 {
     int arr[5] = {1,5,2,4,3};
-    select_sort(arr, 5/*sizeof(int) / sizeof(arr)*/);
+    select_sort(arr, 5);   // sizeof(int) / sizeof(arr)
     for (int i = 0; i < 5; i++) {
         cout << arr[i] << "\t";
     }
