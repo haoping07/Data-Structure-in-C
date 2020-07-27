5d55386c8b2afeb0e1195c7d0bf27639d5464bfb
Hao-Ping Lin <pinglin927@gmail.com>
2020-07-27 21:04:35 +0800

    init commit

---
+++ Searching/binary_search.cpp
@@ -0,0 +1,25 @@
+/*
+§é¥b¬d§ä
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool Binary_Search(vector<int>& a, int key) {
+	int low = 0, high = a.size() - 1;
+	while (low <= high) {
+		int m = low + (high - low) / 2;
+		if (key == a[m]) return true;
+		if (key < a[m]) high = m - 1;
+		else low = m + 1;
+	}
+	return false;
+}
+
+int main()
+{
+	vector<int> a{ 1,3,4,6,7,9,10 };
+	cout << Binary_Search(a, 7) << endl;
+	return 0;
+}
